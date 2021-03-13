aa466081fc1a7df1a81bb975a2cd677daa6f449c
Andy Garcia <andygarcia@live.fr>
2021-03-13 22:08:13 +0100

    ajout du fichier index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 /*---------- PAGE D ACCUEIL ----------*/
 
 /*----- CONSTANTES -----*/
-const fondPhotoOurson = document.getElementById('fond-photo-ourson');
-const picTeddy = document.createElement('img');
-fondPhotoOurson.appendChild(picTeddy);
+
 const catalogue = document.getElementById('catalogue');
 
 /*----- FUNCTIONS -----*/
@@ -103,7 +101,6 @@ function promiseGet() {
 promiseGet()
     .then(function(response) {
 
-        picTeddy.setAttribute('src', 'http://localhost:3000/images/teddy_5.jpg');
         for(let i = 0; i < response.length; i++) {
             const newSection = document.createElement('section');
             catalogue.appendChild(newSection);
